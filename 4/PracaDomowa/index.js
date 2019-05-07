48c6876ca244d3c7e74e5e28a9219a184de21404
Grzegorz Tomaszewski <aroxd3@gmail.com>
2019-05-07 15:02:36 +0200

    zmiana dodawania i usuwania elemntu html po przycisnieciu - usuniecie tru/false

--- 4/PracaDomowa/index.js
+++ 4/PracaDomowa/index.js
@@ -1,19 +1,16 @@
-var test1 = false;
 
 var btn1 = document.getElementById("btn1");
 var myH1;
 var h1;
 
 btn1.addEventListener("click", () => {
-        if(test1 == false){
-             myH1 = document.querySelector("#root"); //pobiera docelowe miejsce elementu
-             h1 = document.createElement("h1");     //tworzy element
-            //h1.id = "h11";                               //nadaje id elementowi
-            myH1.appendChild(h1);                         //wstawia element do drzewa dokumentu DOM
-            test1 = true;
-        }else if(test1 == true){
+        if(document.getElementById("h11")){
             h1.remove();
-            test1 = false;
+        }else{
+            myH1 = document.querySelector("#root"); //pobiera docelowe miejsce elementu
+            h1 = document.createElement("h1");     //tworzy element
+           h1.id = "h11";                               //nadaje id elementowi
+           myH1.appendChild(h1);                         //wstawia element do drzewa dokumentu DOM
         }
 });
 
