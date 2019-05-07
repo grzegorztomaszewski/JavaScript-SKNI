a34f2e31d773ef60bdd82d5560b377939d9a8b7a
Grzegorz Tomaszewski <aroxd3@gmail.com>
2019-05-07 16:01:45 +0200

    cały pierwszy przycisk

--- 4/PracaDomowa/index.js
+++ 4/PracaDomowa/index.js
@@ -1,17 +1,37 @@
 
 var btn1 = document.getElementById("btn1");
-var myH1;
-var h1;
+var btn2 = document.getElementById("btn2");
+var btn3 = document.getElementById("btn3");
+var btn4 = document.getElementById("btn4");
+
+var h1; 
+var h2;
+var myDiv;
+
+
 
 btn1.addEventListener("click", () => {
         if(document.getElementById("h11")){
             h1.remove();
         }else{
-            myH1 = document.querySelector("#root"); //pobiera docelowe miejsce elementu
-            h1 = document.createElement("h1");     //tworzy element
-           h1.id = "h11";                               //nadaje id elementowi
-           myH1.appendChild(h1);                         //wstawia element do drzewa dokumentu DOM
+                    //pobieranie wartości z pliku
+            const xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function(){
+                if(this.readyState == 4 && this.status == 200){
+                    //tworzenie elementu+DOM
+                     myDiv = document.querySelector("#root"); //pobiera docelowe miejsce elementu
+                     h1 = document.createElement("h1");     //tworzy element
+                     h1.id = "h11";                               //nadaje id elementowi
+                     myDiv.appendChild(h1);                         //wstawia element do drzewa dokumentu DOM
+
+                        displayText = this.responseText;
+                        h1.innerText = displayText;
+                    //style
+                        h1.style.color = "red";
+                        h1.style.fontSize = "20px";
+                }
+            };
+            xhttp.open("GET", "plik1txt.txt", true);
+            xhttp.send();
         }
 });
-
-
