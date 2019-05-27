00b6bae6873be7ff8a2300aebd714486e1a76711
Grzegorz Tomaszewski <aroxd3@gmail.com>
2019-05-27 20:29:50 +0200

    JSON dodany

--- case/case.js
+++ case/case.js
@@ -14,3 +14,57 @@
 // Przycisk edytuj na kartach zdjęć ma odblokowywać i blokować 
 // możliwość edycji tytułu i opisu danego zdjęcia
 
+//Pobieranie elementów z HTML
+const body = document.getElementById("body");
+    const counterText = document.getElementById("counterText");
+    const btReset = document.getElementById("btReset");
+    const selectList = document.getElementsByClassName("selectList");
+    const switchDark = document.getElementById("switchDark");
+    const line = document.getElementById("line");
+const generalBox = document.getElementById("generalBox");
+    const box1 = document.getElementById("box1");
+    const foto = document.getElementById("foto");
+    const textOnFoto = document.getElementById("textOnFoto");
+    const fotoText = document.getElementById("fotoText");
+    const description = document.getElementById("description");
+    const btEdit = document.getElementById("btEdit");                   //button Edit
+    const btDelete = document.getElementById("btDelete");               //button Delete
+const boxCreate = document.getElementById("boxCreate");
+    const fotoCreate = document.getElementById("fotoCreate");
+    const btFotoCreate = document.getElementById("btFotoCreate");
+    const fotoTextCreate = document.getElementById("fotoTextCreate");
+    const descriptionCreate = document.getElementById("descriptionCreate");
+    const btCancel = document.getElementById("btCancel");                   //button Edit
+    const btAdd = document.getElementById("btAdd");   
+
+//Slider (zmiana koloru strony na ciemny)
+switchDark.addEventListener("click", () => {
+    if(document.switchDark.checkboxDark.checked == false){
+        body.style.backgroundColor = "darkgray";
+        document.switchDark.checkboxDark.checked = true;
+    }else{
+        body.style.backgroundColor = "white";
+        document.switchDark.checkboxDark.checked = false;
+    }
+});
+
+//Wczytywanie pliku JSON
+const xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+        console.log(this.responseText);
+        //const parsedFile = JSON.parse(this.responseText)
+        //console.log(parsedFile);
+       // alert(parsedFile.texts)
+        // for(let i=0; i< parsedFile.texts.lenght; i++){
+        //     console.log(parsedFile[i]);
+       // }
+    }
+};
+ xhttp.open("GET", "JSON.json", true);
+ xhttp.send();
+
+//Button Edytuj
+btEdit.addEventListener("click", () => {
+
+});
